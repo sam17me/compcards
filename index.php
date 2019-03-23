f7e54e3e53a388c250d30350e684a8cb754f3302
Merrell Gilbert <sam17me@yahoo.com>
2019-03-23 10:29:08 -0500

    Added search button and bar

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 include("_header.php");
 ?>
+<button id='search' name='searchskills' title='search'>Search</button>
+<input type="text"></input>
+<div id = "content1">
+</div>
 </body>
 </html>
